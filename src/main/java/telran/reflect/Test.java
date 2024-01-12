2db4c827d7b8232e5f9de0af899d057d376a6f23
allanteone <allanteone@gmail.com>
2024-01-12 21:43:53 +0200

    cw-50

--- src/main/java/telran/reflect/Test.java
+++ src/main/java/telran/reflect/Test.java
@@ -4,7 +4,7 @@ import java.lang.annotation.*;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Retention(RUNTIME) // Specify the retention policy
+@Retention(RUNTIME)
 @Target(METHOD)
 public @interface Test {
 }
